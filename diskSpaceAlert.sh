620993518d5f61d8e97359b1277aaba06a41ac9c
PrairieWaltz <minnowvswhale@gmail.com>
2024-03-17 19:55:44 -0400

    diskSpace

---
+++ diskSpaceAlert.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script to automatically measure current free space on specified disks and alert if a set capacity is reached #
+
+# "*" indicates value which is system dependent #
+
+filesystems=("/" "/D" "/F")
+usageTotal=$(df -h / | tail -n 1 | awk '{print$6}' | cut -d % -f 1)
+echo "Total usage is: $usageTotal%"
+echo -e "\n"
+
+for i in ${filesystems[@]}; do
+    usage=$(df -h $i | tail -n 1 | awk '{print$6}' | cut -d % -f 1)
+    
+    # if [ $usage -ge 70 ]; then
+    #     alert="Low storage space on $i, usage is: $usage%"
+    #     echo $alert
+    #     echo "Sending out a disk space alert mail."
+    #     echo -e "\n"
+    #     #        echo $alert | mail.mailutils -s "$i is $usage% full. See to it" prairiewaltz@localhost
+    # fi
+done
+
