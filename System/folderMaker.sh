a57c5b443a765ac9af68127fd65bbe1a3fad5847
PrairieWaltz <minnowvswhale@gmail.com>
2024-04-14 12:06:27 -0400

    folder_update

--- System/folderMaker.sh
+++ System/folderMaker.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
 ## Set up a folder structure for a shared network drive, using a file to read paths from txt file ##
-
 ## Create a script that will read the txt file, and create all these folders ##
 
 path="$HOME/folderTest"
-
 echo
 
 while read line;
-
 do
-    
     echo "Working..."
-    
     sleep 1
-    
     mkdir "$path/$line"
-    
     echo "Directory created: $line"
     echo
-    
 done < "$1"
 
+exit 0
